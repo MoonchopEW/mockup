b813ddbcb23fba60e1b919525817d7795476e052
Moonchop <moonchop913@gmail.com>
2022-07-11 17:24:15 +0900

    fix : CORS

--- pages/api/Invoice/card.js
+++ pages/api/Invoice/card.js
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    {
-      res.status(200).json({
-        "data":[
-            { "name": "이 달 예상 수입", "icon": "CashIcon", "amount": "70,000 원" },
-            { "name": "이 달 총 수업 시간",  "icon": "ClockIcon", "amount": "15 시간" },
-            { "name": "이 달 총 수업 수",  "icon": "ClipboardCheckIcon", "amount": "30 번" }
-    ]
-    })
-   }
+
+  {
+    res.header("Access-Control-Allow-Origin","*").status(200).json({
+      data: [
+        { name: "이 달 예상 수입", icon: "CashIcon", amount: "70,000 원" },
+        { name: "이 달 총 수업 시간", icon: "ClockIcon", amount: "15 시간" },
+        {
+          name: "이 달 총 수업 수",
+          icon: "ClipboardCheckIcon",
+          amount: "30 번",
+        },
+      ],
+    });
+  }
 }
