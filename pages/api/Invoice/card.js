4bbfca02a92c9cc41a728288549a4d11b8f13f55
Moonchop <moonchop913@gmail.com>
2022-07-11 17:02:20 +0900

    init

---
+++ pages/api/Invoice/card.js
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    {
+      res.status(200).json({
+        "data":[
+            { "name": "이 달 예상 수입", "icon": "CashIcon", "amount": "70,000 원" },
+            { "name": "이 달 총 수업 시간",  "icon": "ClockIcon", "amount": "15 시간" },
+            { "name": "이 달 총 수업 수",  "icon": "ClipboardCheckIcon", "amount": "30 번" }
+    ]
+    })
+   }
+}
---
+++ pages/api/Invoice/table.js
@@ -0,0 +1,167 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+ {
+   res.status(200).json({
+     data: [
+       {
+         index: "1",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "2",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "3",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "4",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "5",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "6",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "7",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "8",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "9",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "10",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "11",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "12",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+       {
+         index: "13",
+         date: "2022-06-10",
+         setTime: "11:00",
+         time: "2",
+         tutee: "tutees",
+         session: "Tutoring",
+         profit: "500",
+         status: "Complete",
+         image:
+           "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+       },
+     ],
+   });
+ }
+  
+}
