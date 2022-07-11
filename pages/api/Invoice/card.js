b1814ab5322c1b4afc89ca26b24965c4c04f1cda
Moonchop <moonchop913@gmail.com>
2022-07-11 17:33:22 +0900

    fix : CORS

--- pages/api/Invoice/card.js
+++ pages/api/Invoice/card.js
@@ -1,9 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-
-  
-    res.header("Access-Control-Allow-Origin","*").status(200).json({
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+    res.status(200).json({
       data: [
         { name: "이 달 예상 수입", icon: "CashIcon", amount: "70,000 원" },
         { name: "이 달 총 수업 시간", icon: "ClockIcon", amount: "15 시간" },
