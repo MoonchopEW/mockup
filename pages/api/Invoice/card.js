d35412d57ed9b419e0aba2fce6e545dd8da5650c
Moonchop <moonchop913@gmail.com>
2022-07-11 17:37:09 +0900

    fix : CORS

--- pages/api/Invoice/card.js
+++ pages/api/Invoice/card.js
@@ -3,8 +3,6 @@
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
--- pages/api/Invoice/table.js
+++ pages/api/Invoice/table.js
@@ -2,7 +2,17 @@
 
 export default function handler(req, res) {
  {
-   res.header("Access-Control-Allow-Origin","*").status(200).json({
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+   res.status(200).json({
      data: [
        {
          index: "1",
