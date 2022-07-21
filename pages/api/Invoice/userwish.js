e711c705604d980f864529a38491d32476627a30
Moonchop <moonchop913@gmail.com>
2022-07-21 10:34:32 +0900

    add userwish

---
+++ pages/api/Invoice/userwish.js
@@ -0,0 +1,147 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    data: [
+      {
+        id: 1,
+        name: "Useful greeting Phrases",
+        href: "#",
+        price: "0 P",
+        description: "Short & useful phrases for conversation.",
+        options: ["2 Lessons", "Beginner"],
+        rating: 4.6,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Lessons",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/system/lesson/image/package/1/en_thin.jpg",
+        imageAlt:
+          "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+      },
+      {
+        id: 2,
+        name: "Basic Aplhabet (Hangul)",
+        href: "#",
+        price: "0 P",
+        description:
+          "Learn basic alphabet (Hangul). Read and write Hangul like a native.",
+        options: ["12 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Lessons",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/system/lesson/image/package/2/en_thin.jpg",
+        imageAlt: "Front of plain black t-shirt.",
+      },
+      {
+        id: 3,
+        name: "Sofia Hyeon",
+        href: "#",
+        price: "150 P",
+        description: "TOPIK TUTORING",
+        options: ["2 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Tutors",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/user/9304/image/1648525744731.2EC58BAD-61C9-4872-80D9-EB216B0F962F.jpeg",
+        imageAlt: "Front of plain black t-shirt.",
+      },
+      {
+        id: 4,
+        name: "Useful greeting Phrases",
+        href: "#",
+        price: "$256",
+        description: "Short & useful phrases for conversation.",
+        options: ["2 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Lessons",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/system/lesson/image/package/1/en_thin.jpg",
+        imageAlt:
+          "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+      },
+      {
+        id: 5,
+        name: "Basic Aplhabet (Hangul)",
+        href: "#",
+        price: "$32",
+        description:
+          "Learn basic alphabet (Hangul). Read and write Hangul like a native.",
+        options: ["2 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Lessons",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/system/lesson/image/package/2/en_thin.jpg",
+        imageAlt: "Front of plain black t-shirt.",
+      },
+      {
+        id: 6,
+        name: "Sofia Hyeon",
+        href: "#",
+        price: "$32",
+        description: "TOPIK TUTORING",
+        options: ["2 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Tutors",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/user/9304/image/1648525744731.2EC58BAD-61C9-4872-80D9-EB216B0F962F.jpeg",
+        imageAlt: "Front of plain black t-shirt.",
+      },
+      {
+        id: 7,
+        name: "Useful greeting Phrases",
+        href: "#",
+        price: "$256",
+        description: "Short & useful phrases for conversation.",
+        options: ["2 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Lessons",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/system/lesson/image/package/3/en_thin.jpg",
+        imageAlt:
+          "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+      },
+      {
+        id: 8,
+        name: "Basic Aplhabet (Hangul)",
+        href: "#",
+        price: "$32",
+        description:
+          "Learn basic alphabet (Hangul). Read and write Hangul like a native.",
+        options: ["2 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Lessons",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/system/lesson/image/package/4/en_thin.jpg",
+        imageAlt: "Front of plain black t-shirt.",
+      },
+      {
+        id: 9,
+        name: "Sofia Hyeon",
+        href: "#",
+        price: "$32",
+        description: "TOPIK TUTORING",
+        options: ["2 Lessons", "Beginner"],
+        rating: 5.0,
+        eyeHeartThumb: [373, 4.8, 13],
+        session: "Tutors",
+        active: true,
+        imageSrc:
+          "https://s3.ap-northeast-2.amazonaws.com/resource.ebridgehub.com/user/9304/image/1648525744731.2EC58BAD-61C9-4872-80D9-EB216B0F962F.jpeg",
+        imageAlt: "Front of plain black t-shirt.",
+      },
+    ],
+  });
+}
